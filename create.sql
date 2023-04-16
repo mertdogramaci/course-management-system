b8f4d680f31a74597ae5428c7cb84f96de52f990
mertdogramaci <mertdogramaci@gmail.com>
2023-04-16 23:51:40 +0300

    tried db connection

--- create.sql
+++ create.sql
@@ -1,5 +1,12 @@
-create table user (
-    ID int not null,
-    name varchar(255),
-    surname varchar(255)
-)
+CREATE DATABASE IF NOT EXISTS course_management_system;
+USE course_management_system;
+
+CREATE TABLE IF NOT EXISTS user (
+    ID INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+insert into user (ID, name, surname) values (1, 'Mert', 'Doğramacı');
+select * from user;
