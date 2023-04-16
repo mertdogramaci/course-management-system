a4ac3bc8137f312ed6ac9175daa171a881e1baac
mertdogramaci <mertdogramaci@gmail.com>
2023-04-17 00:42:00 +0300

    re-edited for postgre

--- create.sql
+++ create.sql
@@ -1,12 +1,10 @@
-CREATE DATABASE IF NOT EXISTS course_management_system;
-USE course_management_system;
-
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS users (
     ID INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
     PRIMARY KEY (ID)
 );
 
-insert into user (ID, name, surname) values (1, 'Mert', 'Doğramacı');
-select * from user;
+insert into users (ID, name, surname) values (1, 'Mert', 'Doğramacı');
+select * from users;
+delete from users where id = 1;
