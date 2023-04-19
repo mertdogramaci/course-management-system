8a53f7066b5eeab374c5c380aa1ad0e230311ff3
mertdogramaci <mertdogramaci@gmail.com>
2023-04-20 00:04:28 +0300

    yatmadan önce son hal

--- create.sql
+++ create.sql
@@ -1,4 +1,6 @@
 -- Drop all tables for being able to re-run this query
+DROP TABLE IF EXISTS user_has_login_credentials;
+DROP TABLE IF EXISTS user_has_contact_info;
 DROP TABLE IF EXISTS login_credentials;
 DROP TABLE IF EXISTS contact_info;
 DROP TABLE IF EXISTS submission;
@@ -15,25 +17,22 @@ DROP TABLE IF EXISTS faculty;
 
 
 CREATE TABLE faculty (
-    ID INT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (ID)
+    ID INT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE department (
-    ID INT NOT NULL,
+    ID INT PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
     facultyID INT NOT NULL,
-    PRIMARY KEY (ID),
     FOREIGN KEY (facultyID) REFERENCES faculty(ID)
 );
 
 CREATE TABLE users (
-    ID INT NOT NULL,
+    ID INT PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
     surname VARCHAR(255) NOT NULL,
     departmentID INT NOT NULL,
-    PRIMARY KEY (ID),
     FOREIGN KEY (departmentID) REFERENCES department(ID)
 );
 
@@ -51,39 +50,36 @@ CREATE TABLE instructor (
 ) INHERITS (users);
 
 CREATE TABLE contact_info (
-    ID INT NOT NULL,
+    ID INT PRIMARY KEY NOT NULL,
     phone INT,
     email VARCHAR(255),
     address VARCHAR(255),
     userID INT NOT NULL,
-    PRIMARY KEY (ID),
     FOREIGN KEY (userID) REFERENCES users(ID)
 );
 
 CREATE TABLE login_credentials(
-    ID INT NOT NULL,
+    ID INT PRIMARY KEY NOT NULL,
     -- username = int de olabilir ben instructor username adı.soyadı şeklinde olacak diye düşündüm
     username VARCHAR(255),
     password VARCHAR(255) NOT NULL,
     userID INT NOT NULL,
-    PRIMARY KEY (ID),
     FOREIGN KEY (userID) REFERENCES users(ID)
 );
 
 CREATE TABLE course(
-    ID INT NOT NULL,
+    ID INT PRIMARY KEY NOT NULL,
     title VARCHAR(255) NOT NULL,
     ects INT NOT NULL,
     -- type için belki boolean gibi bir şey kullanılabilir
     type VARCHAR(255) NOT NULL,
     description TEXT,
     departmentID INT NOT NULL,
-    PRIMARY KEY (ID),
     FOREIGN KEY (departmentID) REFERENCES department(ID)
 );
 
 CREATE TABLE section(
-    ID INT NOT NULL,
+    ID INT PRIMARY KEY NOT NULL,
     -- semester için belki boolean gibi bir şey kullanılabilir veya int daha iyi bool yerine
     semester VARCHAR(255) NOT NULL,
     year INT NOT NULL,
@@ -91,40 +87,57 @@ CREATE TABLE section(
     quota INT NOT NULL,
     courseID INT NOT NULL,
     instructorID INT NOT NULL,
-    PRIMARY KEY (ID),
     FOREIGN KEY (courseID) REFERENCES course(ID),
     FOREIGN KEY (instructorID) REFERENCES instructor(ID)
 );
 
 CREATE TABLE homework(
-    ID INT NOT NULL,
+    ID INT PRIMARY KEY NOT NULL,
     title TEXT NOT NULL,
     content BYTEA,
     issueDate DATE NOT NULL,
     dueDate DATE NOT NULL,
     sectionID INT NOT NULL,
-    PRIMARY KEY (ID),
     FOREIGN KEY (sectionID) REFERENCES section(ID)
 );
 
 CREATE TABLE submission(
-    ID INT NOT NULL,
+    ID INT PRIMARY KEY NOT NULL,
     marks INT,
     content BYTEA,
     studentID INT NOT NULL,
     homeworkID INT NOT NULL,
-    PRIMARY KEY (ID),
     FOREIGN KEY (studentID) REFERENCES student(ID),
     FOREIGN KEY (homeworkID) REFERENCES homework(ID)
 );
 
 CREATE TABLE time_slot(
-    ID INT NOT NULL,
+    ID INT PRIMARY KEY NOT NULL,
     -- semester için belki int gibi bir şey kullanılabilir
     day VARCHAR(255) NOT NULL,
     startTime TIMESTAMP NOT NULL,
     endTime TIMESTAMP NOT NULL,
     sectionID INT NOT NULL,
-    PRIMARY KEY (ID),
     FOREIGN KEY (sectionID) REFERENCES section(ID)
+);
+
+CREATE TABLE user_has_contact_info (
+    userID INT NOT NULL,
+    contact_infoID INT NOT NULL,
+    PRIMARY KEY (userID, contact_infoID),
+    FOREIGN KEY (userID) REFERENCES users(ID)
+);
+
+CREATE TABLE user_has_login_credentials(
+    loginCredentialsID INT NOT NULL,
+    userID INT NOT NULL,
+    PRIMARY KEY (loginCredentialsID, userID)
+);
+
+CREATE TABLE student_enrolls_section(
+    studentID
+    sectionID
+    grade
+    nonAttendanceCount
+    isApproved
 );
