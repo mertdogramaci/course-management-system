b6c9c9d5ef04249ca1db51e94cbddc7ac6a4757f
mertdogramaci <mertdogramaci@gmail.com>
2023-04-19 17:51:46 +0300

    biraz eklendi ama yoruldum

---
+++ insert.sql
@@ -0,0 +1,57 @@
+INSERT INTO faculty (ID, name) VALUES (1, 'Engineering');
+INSERT INTO faculty (ID, name) VALUES (2, 'Medical School');
+INSERT INTO faculty (ID, name) VALUES (3, 'Foreign Languages');
+INSERT INTO faculty (ID, name) VALUES (4, 'Sport Science');
+INSERT INTO faculty (ID, name) VALUES (5, 'Law');
+INSERT INTO faculty (ID, name) VALUES (6, 'Economics and Adminidtrative Sciences');
+INSERT INTO faculty (ID, name) VALUES (7, 'Pharmacy');
+INSERT INTO faculty (ID, name) VALUES (8, 'Science and Literature');
+INSERT INTO faculty (ID, name) VALUES (9, 'Fine Arts');
+INSERT INTO faculty (ID, name) VALUES (10, 'Communication');
+
+
+INSERT INTO department (ID, name, facultyID) VALUES (1, 'Computer Engineering', 1);
+INSERT INTO department (ID, name, facultyID) VALUES (2, 'Electrical and Electronic Engineering', 1);
+INSERT INTO department (ID, name, facultyID) VALUES (3, 'Industrial Engineering', 1);
+INSERT INTO department (ID, name, facultyID) VALUES (4, 'Medicine', 2);
+INSERT INTO department (ID, name, facultyID) VALUES (5, 'Modern Languages', 3);
+INSERT INTO department (ID, name, facultyID) VALUES (6, 'Physical Education and Sports Teaching', 4);
+INSERT INTO department (ID, name, facultyID) VALUES (7, 'Civil Law', 5);
+INSERT INTO department (ID, name, facultyID) VALUES (8, 'Economics', 6);
+INSERT INTO department (ID, name, facultyID) VALUES (9, 'Business Administration', 6);
+INSERT INTO department (ID, name, facultyID) VALUES (10, 'International Relations', 6);
+INSERT INTO department (ID, name, facultyID) VALUES (11, 'Public Finance', 6);
+INSERT INTO department (ID, name, facultyID) VALUES (12, 'Pharmacology', 7);
+INSERT INTO department (ID, name, facultyID) VALUES (13, 'Statistics', 8);
+INSERT INTO department (ID, name, facultyID) VALUES (14, 'Physics', 8);
+INSERT INTO department (ID, name, facultyID) VALUES (15, 'Psychology', 8);
+INSERT INTO department (ID, name, facultyID) VALUES (16, 'Cartoon and Animation', 9);
+INSERT INTO department (ID, name, facultyID) VALUES (17, 'Radio, TV and Film', 10);
+
+
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (1, 'Mert', 'Doğramacı', 1, 21946055, '2019-08-21', 40);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (2, 'Özgün', 'Akyüz', 1, 21827005, '2018-08-10', 40);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (3, 'Emre', 'Akdeniz', 4, 21945102, '2019-08-10', 40);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (4, 'Eser', 'Vatansever', 2, 21947727, '2019-08-10', 40);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (5, 'Abdurrahman', 'Özçağlayan', 4, 21296013, '2012-08-10', 40);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (6, 'Efe', 'Çelik', 3, 2200356833, '2020-07-28', 30);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (7, 'Tuğçe', 'Dönmez', 5, 2212345678, '2021-07-15', 30);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (8, 'Ayşe Buket', 'Yağız', 6, 2214545678, '2021-08-18', 30);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (9, 'İrem', 'Yağız', 15, 2202345678, '2020-07-15', 30);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (10, 'Osman', 'Portakal', 7, 2222345678, '2022-08-21', 30);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (11, 'Damla', 'Özdağ', 8, 2202345678, '2020-08-21', 30);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (12, 'Emrah', 'Duman', 9, 21745102, '2017-08-10', 40);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (13, 'Melek', 'Baykal', 10, 21645102, '2016-08-10', 40);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (14, 'Özge', 'Burak', 11, 21945102, '2019-07-10', 40);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (15, 'Alperen', 'Doğru', 12, 21845102, '2018-08-20', 40);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (16, 'Ali', 'Veli', 13, 21845102, '2018-07-10', 40);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (17, 'Özge', 'Burak', 14, 21945102, '2019-07-10', 40);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (18, 'Burcu', 'Çiçek', 16, 2212895678, '2021-07-10', 30);
+INSERT INTO student (ID, name, surname, departmentID, studentID, schoolEnrollmentDate, semesterECTS) VALUES (19, 'Almila', 'Dağlar', 17, 2200895678, '2020-08-12', 30);
+
+
+INSERT INTO instructor (ID, name, surname)
+
+select * 
+from student join department on (student.departmentID = department.ID) 
+join faculty on (faculty.ID = department.facultyID);
