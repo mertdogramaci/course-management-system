da1b30fdca556c69c804fcc3fb1d5dde69b5479d
b21827005 <b21827005@cs.hacettepe.edu.tr>
2023-06-05 19:36:27 +0300

    AuthorityRepository jpa removed

--- src/backend/src/main/java/com/group20/coursemanagementsystem/security/repositories/AuthorityRepository.java
+++ src/backend/src/main/java/com/group20/coursemanagementsystem/security/repositories/AuthorityRepository.java
@@ -1,9 +1,42 @@
 package com.group20.coursemanagementsystem.security.repositories;
 
+import com.group20.coursemanagementsystem.request.EnrolmentRequest;
 import com.group20.coursemanagementsystem.security.domain.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-    boolean existsByAuthority(String authority);
-    Authority findByAuthority(String authority);
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public class AuthorityRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional
+    public <A extends Authority> A save(A authority) {
+        Query query = entityManager.createNativeQuery("INSERT INTO authority (authority) VALUES (?)");
+        query.setParameter(1, authority.getAuthority());
+        query.executeUpdate();
+
+        return authority;
+    }
+
+    public Authority findByAuthority(String authority) {
+        Query query = entityManager.createQuery("SELECT a FROM Authority a WHERE a.authority = :authority", Authority.class);
+        query.setParameter("authority", authority);
+        List resultList = query.getResultList();
+
+        if (resultList.isEmpty()) { return null; }
+
+        return (Authority) resultList.get(0);
+    }
+
+    public boolean existsByAuthority(String authority) {
+        return findByAuthority(authority) != null;
+    }
 }
