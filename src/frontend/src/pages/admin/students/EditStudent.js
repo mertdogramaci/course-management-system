15d95b7c5cf858bf020d15ce7000515e807683f7
Mert Doğramacı <mertdogramaci@gmail.com>
2023-06-13 04:07:33 +0300

    added section list for student

---
+++ src/frontend/src/pages/admin/students/EditStudent.js
@@ -0,0 +1,108 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams, Link } from "react-router-dom";
+import { Button, Container, Form, FormGroup, Input, Label } from 'reactstrap';
+import ApiRoutes from '../../../api/routes';
+import axios from '../../../api/axios';
+
+function EditStudent() {
+    const initialStudentState = {
+        address: "",
+        email: "",
+        firstName: "",
+        hacettepeID: "",
+        id: "",
+        lastName: "",
+        memberType: "",
+        phoneNumber: "",
+        semesterECTS: "",
+    };
+
+    const [student, setStudent] = useState(initialStudentState);
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+    useEffect(
+        () => {
+            fetchStudent();
+        }, [id]
+    );
+
+    const fetchStudent = async () => {
+        try {
+            const response = await axios.get(ApiRoutes.MEMBER + "/findStudent/" + id);
+
+            if (response.status === 200) {
+                const fetchedStudent = response.data;
+
+                console.log(response.data);
+
+                if (JSON.stringify(fetchedStudent) !== JSON.stringify(student)) {
+                    setStudent(fetchedStudent);
+                }
+            }
+        } catch (error) {
+            console.log("error!!");
+        }
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setStudent((prevState) => {
+            return {
+                ...prevState, [name]: value,
+            };
+        });
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        
+        await axios.put(ApiRoutes.MEMBER + id, student);
+        
+        navigate('/members/fetchAllMembers');
+    }
+
+    const title = <h2>Edit User</h2>;
+
+    return (
+        <div>
+            <Container>
+                {title}
+                <Form onSubmit={handleSubmit}>
+                    <FormGroup>
+                        <Label for="firstName">Name</Label>
+                        <Input type="text" name="firstName" id="firstName" value={student.firstName}
+                            onChange={handleChange} autoComplete="name" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="lastName">Surname</Label>
+                        <Input type="text" name="lastName" id="lastName" value={student.lastName || ''}
+                            onChange={handleChange} autoComplete="surname" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email">Email</Label>
+                        <Input type="text" name="email" id="email" value={student.email || ''}
+                            onChange={handleChange} autoComplete="email" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="phoneNumber">Phone</Label>
+                        <Input type="text" name="phoneNumber" id="phoneNumber" value={student.phoneNumber || ''}
+                            onChange={handleChange} autoComplete="phone" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="hacettepeID">Hacettepe ID</Label>
+                        <Input type="text" name="hacettepeID" id="hacettepeID" value={student.hacettepeID || ''}
+                            onChange={handleChange} autoComplete="hacettepeID" />
+                    </FormGroup>
+                    <FormGroup>
+                        <Button color="primary" type="submit">Save</Button>{' '}
+                        <Button color="secondary" tag={Link} to="/members/fetchAllMembers">Cancel</Button>
+                    </FormGroup>
+                </Form>
+            </Container>
+        </div>
+    )
+}
+
+export default EditStudent;
--- src/frontend/src/pages/admin/students/StudentTable.js
+++ src/frontend/src/pages/admin/students/StudentTable.js
@@ -4,15 +4,15 @@ import { Button, ButtonGroup } from "reactstrap";
 
 function StudentTable(probs) {
     function remove(id) {
-        fetch(`/students/${id}`, {
-          method: 'DELETE',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          }
+        fetch(`/members/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
         }).then(() => {
-          const updatedStudents = [...probs.students].filter(i => i.id !== id);
-          probs.setStudents(updatedStudents);
+            const updatedStudents = [...probs.students].filter(i => i.id !== id);
+            probs.setStudents(updatedStudents);
         });
     }
 
@@ -24,6 +24,7 @@ function StudentTable(probs) {
                     <th>First Name</th>
                     <th>Last Name</th>
                     <th>Email</th>
+                    <th>Phone Number</th>
                     <th>Student ID</th>
                     <th>School Enrollment Date</th>
                     <th>Faculty</th>
@@ -39,12 +40,19 @@ function StudentTable(probs) {
                             <th>{student.firstName}</th>
                             <th>{student.lastName}</th>
                             <th>{student.email}</th>
+                            <th>{student.phoneNumber}</th>
                             <th>{student.hacettepeID}</th>
                             <th>{student.schoolEnrollmentDate}</th>
                             <th>{(student.department === undefined) ? null : student.department.faculty.name}</th>
                             <th>{(student.department === undefined) ? null : student.department.name}</th>
                             <th>
-                                
+                                <ButtonGroup>
+                                    <Button size="sm" color="primary" tag={Link} to={"/studentEnrollsSection/" + student.id}>Show Enrolled Sections</Button>
+                                    &ensp;
+                                    <Button size="sm" color="primary" tag={Link} to={"/members/" + student.id}>Edit</Button>
+                                    &ensp;
+                                    <Button size="sm" color="primary" onClick={() => remove(student.id)}>Delete</Button>
+                                </ButtonGroup>
                             </th>
                         </tr>
                     );
